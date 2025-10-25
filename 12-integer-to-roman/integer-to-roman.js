0fe562b094f75c0b273ca0fabb9228496441385c
Kodirov8788@gmail.com <63976402+kodirov8788@users.noreply.github.com>
2025-10-25 16:48:36 +0900

    Time: 2 ms (95.70%) | Memory: 58.4 MB (99.91%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+function intToRoman(num) {
+    const values = [
+        1000, 900, 500, 400, 100, 90, 50, 40,
+        10, 9, 5, 4, 1
+    ];
+    const symbols = [
+        "M", "CM", "D", "CD", "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV", "I"
+    ];
+    let roman = "";
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            roman += symbols[i];
+            num -= values[i];
+        }
+    }
+    return roman;
+}
